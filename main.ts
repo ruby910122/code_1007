ba0b2c7d7bce5474999c89e1268e65442a11e1b8
ruby910122 <71751285+ruby910122@users.noreply.github.com>
2020-10-07 15:30:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+for (let index = 0; index <= 4; index++) {
+    led.plot(0, index)
+    led.plot(4, index)
+    led.plot(index, 0)
+    led.plot(index, 4)
+    basic.pause(500)
+}
